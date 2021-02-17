dafa7d15e6481715033389a023e6191ccbe958c7
tylerT <nguyenkhanhtoannk812@gmail.com>
2021-02-17 15:02:15 +0700

    Change all models server

---
+++ ACFIP.Data/Helpers/AppUtils.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACFIP.Data.Helpers
+{
+    public class AppUtils
+    {
+        // hash function, using SHA512
+        public static string hashSHA512(string password, byte[] salt)
+        {
+            return Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA512,
+                iterationCount: 10,
+                numBytesRequested: 512 / 8));
+        }
+        public static bool VerifyPassword(string userEnteredPassword, string dbPasswordHash, byte[] salt)
+        {
+            string hashedPassword = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: userEnteredPassword,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA512,
+                iterationCount: 10,
+                numBytesRequested: 512 / 8));
+            return dbPasswordHash == hashedPassword;
+        }
+    }
+}
