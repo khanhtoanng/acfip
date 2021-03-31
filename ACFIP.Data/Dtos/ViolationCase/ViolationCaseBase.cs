a29d39045925a77049d18b9a073f2e3e49351933
tylerT <nguyenkhanhtoannk812@gmail.com>
2021-03-31 07:56:59 +0700

    .

---
+++ ACFIP.Data/Dtos/ViolationCase/ViolationCaseBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACFIP.Data.Dtos.ViolationCase
+{
+    public class ViolationCaseBase
+    {
+        public int Id { get; set; }
+        public string ImgUrl { get; set; }
+        public string VideoUrl { get; set; }
+        public int Status { get; set; }
+        public bool IsView { get; set; }
+        public string FileName { get; set; }
+
+    }
+}
